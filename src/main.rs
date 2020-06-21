62157708dc08da330cba2997e07beb50c6568fa1
Nathan Stocks <cleancut@github.com>
2020-06-21 14:38:12 -0600

    Add bounce collisions and death by off-the-edge

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use legion::prelude::*;
 use rand::prelude::*;
 use rusty_engine::gfx::event::{ButtonProcessor, GameEvent};
 use rusty_engine::gfx::{color::Color, Sprite, Window};
-use rusty_engine::glm::{distance2, Vec2};
+use rusty_engine::glm::{cross, distance, distance2, reflect_vec, Vec2};
+
+const GOAL_RADIUS: f32 = 1. / 8.;
+const OBSTACLE_RADIUS: f32 = 1. / 12.;
+const PLAYER_RADIUS: f32 = 1. / 16.;
 
 type Position = Vec2;
 struct Velocity(Vec2);
@@ -29,7 +33,7 @@ fn main() {
             Position::new(0., 0.), // Ignored
             0.,
             1.,
-            1. / 8.,
+            GOAL_RADIUS,
             Color::new(0., 1., 0.),
         ),
         // Player circle (small-ish, blue)
@@ -38,7 +42,7 @@ fn main() {
             Position::new(0., 0.), // Ignored
             0.,
             1.,
-            1. / 16.,
+            PLAYER_RADIUS,
             Color::new(0., 0., 1.),
         ),
         // Obstacle circles -- reusing the same sprite for all instances is probably a terrible idea, but I want to try it
@@ -47,7 +51,7 @@ fn main() {
             Position::new(0., 0.), // Ignored
             0.,
             1.,
-            1. / 12.,
+            OBSTACLE_RADIUS,
             Color::new(1., 0., 0.),
         ),
     ];
@@ -87,6 +91,8 @@ fn main() {
 
     let mut button_processor = ButtonProcessor::new();
     'gameloop: loop {
+        let mut dead = false;
+
         for event in window.poll_game_events() {
             match event {
                 GameEvent::Quit => break 'gameloop,
@@ -99,20 +105,68 @@ fn main() {
         }
         window.drawstart();
 
-        // Physics stuff
+        // Get the player's position
+        let mut player_pos = Position::new(0., 0.);
+        for pos in <Read<Position>>::query()
+            .filter(tag_value(&Player))
+            .iter(&mut world)
+        {
+            player_pos = *pos;
+        }
+
+        // Detect Collision
+        let mut maybe_collision = None;
+        for pos in <Read<Position>>::query()
+            .filter(tag_value(&Obstacle))
+            .iter(&mut world)
+        {
+            if distance(&player_pos, &*pos) < PLAYER_RADIUS + OBSTACLE_RADIUS {
+                maybe_collision = Some(*pos);
+            }
+        }
+
+        // Adjust player velocity
         for (mut pos, mut vel) in <(Write<Position>, Write<Velocity>)>::query()
             .filter(tag_value(&Player))
             .iter(&mut world)
         {
-            let max_vel = 0.0005;
+            // Player's new velocity based on previous velocity and current input
+            let max_vel = 0.0008;
+            let bounce_vel = 0.0015;
             let velocity_scale = 0.000005;
             let drag = 0.999;
-            (*vel).0 += button_processor.direction * velocity_scale;
+            // Apply drag first
             (*vel).0 *= drag;
+            let magnitude_before = (*vel).0.magnitude();
+            (*vel).0 += button_processor.direction * velocity_scale;
+            // If we're over max velocity, clamp velocity so input only affects post-drag direction
             if (*vel).0.magnitude() > max_vel {
-                (*vel).0 = (*vel).0.normalize() * max_vel;
+                (*vel).0 = (*vel).0.normalize() * magnitude_before;
             }
-            *pos = *pos + (*vel).0;
+
+            // Collision?
+            if let Some(collision_pos) = maybe_collision {
+                // Reflect velocity & boost it upon collision
+                let normal_vector = (collision_pos - *pos).normalize();
+                let surface_vector = Vec2::new(-normal_vector[1], normal_vector[0]);
+                let new_velocity =
+                    -reflect_vec(&((*vel).0), &surface_vector).normalize() * bounce_vel;
+                (*vel).0 = new_velocity;
+            }
+
+            // Update position
+            let new_pos = *pos + (*vel).0;
+            *pos = new_pos;
+
+            // Death by edge?
+            if new_pos[0] < -1. - PLAYER_RADIUS
+                || new_pos[0] > 1. + PLAYER_RADIUS
+                || new_pos[1] < -1. - PLAYER_RADIUS
+                || new_pos[1] > 1. + PLAYER_RADIUS
+            {
+                println!("DEAD");
+                dead = true;
+            }
         }
 
         // Draw the Goal
@@ -146,5 +200,9 @@ fn main() {
         }
 
         window.drawfinish();
+
+        if dead {
+            break 'gameloop;
+        }
     }
 }
